772030dfa7fe6a6fb5718a4963cfabceb88097ec
Gurleen <gurleendhillonm1114@gmail.com>
2021-05-29 16:29:48 +0530

    spo2 in main schema

--- app/schema.py
+++ app/schema.py
@@ -5,13 +5,14 @@ import water.schema
 import bp.schema
 import sleep.schema
 import cholestrol.schema
+import spo2.schema
 
 
-class Query(users.schema.Query, meal.schema.Query,water.schema.Query,bp.schema.Query,sleep.schema.Query, cholestrol.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, meal.schema.Query,water.schema.Query,bp.schema.Query,sleep.schema.Query, cholestrol.schema.Query,spo2.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(users.schema.Mutation, meal.schema.Mutation,water.schema.Mutation,bp.schema.Mutation,sleep.schema.Mutation, cholestrol.schema.Mutation, graphene.ObjectType):
+class Mutation(users.schema.Mutation, meal.schema.Mutation,water.schema.Mutation,bp.schema.Mutation,sleep.schema.Mutation, cholestrol.schema.Mutation,spo2.schema.Mutation, graphene.ObjectType):
     pass
 
 
